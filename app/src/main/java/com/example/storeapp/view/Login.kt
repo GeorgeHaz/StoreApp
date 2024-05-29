899e6e0e2e9f75dd012d18312d24b58b087eb4f1
George Haz <george.haz@yahoo.com>
2024-05-29 17:20:06 -0500

    IMPLEMENTACION DE TEMAS DE COLORES

--- app/src/main/java/com/example/storeapp/view/Login.kt
+++ app/src/main/java/com/example/storeapp/view/Login.kt
@@ -26,18 +26,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -51,15 +50,16 @@ fun Login(loginViewModel: LoginViewModel, navController: NavController) {
     Surface(
         modifier = Modifier.fillMaxSize()
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.fondo),
-            contentDescription = "background",
-            modifier = Modifier
-                .fillMaxSize()
-        )
         Box(
             modifier = Modifier.fillMaxSize()
         ) {
+            Image(
+                painter = painterResource(id = R.drawable.fondo),
+                contentDescription = "background",
+                modifier = Modifier
+                    .fillMaxSize(),
+                contentScale = ContentScale.Crop
+            )
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center,
@@ -82,13 +82,13 @@ fun LoginBody(modifier: Modifier) {
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier
+        modifier = Modifier
     ) {
         Image(
             painter = painterResource(id = R.drawable.logo_fakestore),
             contentDescription = "app logo",
             modifier = Modifier
-                .size(128.dp)
+                .size(200.dp)
         )
     }
 }
